04c33bbd29503d6d87e829176fb90dafc5016a6e
zaing123 <130922337+zaing123@users.noreply.github.com>
2024-12-31 06:16:35 -0800

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,30 +8,56 @@ namespace main1234.Controllers
         {
             return View();
         }
+
         public IActionResult Post()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult Contact()
         {
+            ViewBag.Message = "Please fill out the contact form.";
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Contact(string name, string email, string message)
+        {
+            if (name == "zain" && email == "zainfarooq520@gmail.com")
+            {
+                ViewData["Response"] = "You have successfully logged into the system!";
+                TempData["FormSubmitted"] = true; // Hides the form after submission
+            }
+            else
+            {
+                TempData["Alert"] = "Your email or username may not be correct!";
+                ViewData["Response"] = "Please try again.";
+            }
+
+            return View();
+        }
+
         public IActionResult Music()
         {
             return View();
         }
+
         public IActionResult Sports()
         {
             return View();
         }
+
         public IActionResult Conference()
         {
             return View();
         }
+
         public IActionResult Culture()
         {
             return View();
         }
+
         public IActionResult Other()
         {
             return View();
