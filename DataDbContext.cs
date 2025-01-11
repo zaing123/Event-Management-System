895b2cd1fe7d2e8c47e745e22371ba24b28ba7cf
zaing123 <130922337+zaing123@users.noreply.github.com>
2025-01-11 02:14:12 -0800

    Add files via upload

---
+++ DataDbContext.cs
@@ -0,0 +1,15 @@
+﻿using main1234.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace main1234.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext (DbContextOptions<DataDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactModel> ContactModel { get; set; }
+        public DbSet<PostModel> PostModel { get; set; }
+    }
+}
